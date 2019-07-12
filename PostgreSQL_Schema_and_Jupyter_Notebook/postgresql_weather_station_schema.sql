76e71888b2365dcf41717edb1f69f3481d22162c
Josefina Aurea Amaro <josie.a.amaro@gmail.com>
2019-07-12 15:30:50 -0400

    to update schema and notebook with bonus code

--- PostgreSQL_Schema_and_Jupyter_Notebook/postgresql_weather_station_schema.sql
+++ PostgreSQL_Schema_and_Jupyter_Notebook/postgresql_weather_station_schema.sql
@@ -2,9 +2,12 @@ CREATE TABLE weather_stations (
   id SERIAL PRIMARY KEY,
   station VARCHAR(12) NOT NULL,
   name VARCHAR(50) NOT NULL,
-  latitude INT NOT NULL,
-  longitude INT NOT NULL,
-  elevation INT NOT NULL
+  latitude VARCHAR(20) NOT NULL,
+  longitude VARCHAR(20) NOT NULL,
+  elevation VARCHAR(20) NOT NULL
 );
 
 SELECT * from weather_stations;
+
+ALTER TABLE weather_stations
+ALTER COLUMN elevation TYPE VARCHAR(20);
